a3484152caaf9dd0769b2140a9e5f68d979f49dc
Md Sakib Uddin <md.sakib.uddin@g.bracu.ac.bd>
2025-08-25 21:44:55 +1000

    something

--- js/home.js
+++ js/home.js
@@ -1,13 +1,32 @@
+//Reusable Code for parseINt
+function getInputValueNumber(id) {
+  const inputAmount = parseInt(document.getElementById(id).value);
+  return inputAmount;
+}
+
+//Reusable code for getting value
+
+function getValue(id) {
+  const input = document.getElementById(id).value;
+  return input;
+}
+// Reusable code for innerText
+function getInnerText(id) {
+  const innerText = parseInt(document.getElementById(id).innerText);
+  return innerText;
+}
+//Add money feature
+
 let validPin = 2552;
 
 document
   .getElementById("btn-add-money")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    const bank = document.getElementById("bank").value;
-    const accountNumber = document.getElementById("account-number").value;
-    const amount = parseFloat(document.getElementById("add-amount").value);
-    const pin = parseInt(document.getElementById("add-pin").value);
+    const bank = getValue("bank");
+    const accountNumber = getValue("account-number");
+    const amount = getInputValueNumber("add-amount");
+    const pin = getInputValueNumber("add-pin");
     //Checking account number is correct or not
     if (accountNumber.length < 11) {
       alert("Please provide a valid 11 digit account number");
@@ -24,3 +43,29 @@ document
     document.getElementById("available-balance").innerText =
       totalNewAvailableBalance;
   });
+
+//Cashout Money Feature
+
+document
+  .getElementById("btn-withdraw")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const agentNumber = getValue("agent-number");
+    const cashoutAmount = getValue("cashout-amount");
+    const cashoutPin = getValue("cashout-pin");
+    const availableBalance = getInnerText("available-balance");
+    const totalNewAvailableBalance = availableBalance - cashoutAmount;
+    document.getElementById("available-balance").innerText =
+      totalNewAvailableBalance;
+  });
+
+//Toggling Feature
+document.getElementById("add-money-btn").addEventListener("click", function () {
+  document.getElementById("cash-out-parent").style.display = "none";
+  document.getElementById("add-money-parent").style.display = "block";
+});
+
+document.getElementById("btn-cash-out").addEventListener("click", function () {
+  document.getElementById("add-money-parent").style.display = "none";
+  document.getElementById("cash-out-parent").style.display = "block";
+});
